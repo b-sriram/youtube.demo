be573c4f22496d5a361b93302312191f5cc7ea01
bigc <bigc@bigc.(none)>
2014-02-28 17:21:42 +0530

    first commit

---
+++ sampleee1.php
@@ -0,0 +1,11 @@
+<html>
+    <p>
+	<?php
+	ini_set('display_errors', 1);
+	// Create an array with several elements in it,
+	// then sort it and print the joined elements to the screen
+    $priya=array("is","a","mad","boy");
+	 print join(" ", $priya);
+	?>
+	</p>
+</html>
